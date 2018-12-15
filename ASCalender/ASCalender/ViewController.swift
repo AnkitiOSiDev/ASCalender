f495ee65b38bd990c828035c97df35fd63d9f252
Ankit <ankitshrivastavano.2@gmail.com>
2018-12-16 00:04:48 +0530

    Code for initial frame settings

--- ASCalender/ASCalender/ViewController.swift
+++ ASCalender/ASCalender/ViewController.swift
@@ -21,18 +21,23 @@ class ViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        calenderDateView = Bundle.main.loadNibNamed("ASCalenderView", owner: self, options: nil)?.first as? ASCalenderView
-        calenderDateView.backgroundColor = UIColor.darkGray
-        calenderDateView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: calenderView.frame.size.height)
-        calenderDateView?.translatesAutoresizingMaskIntoConstraints = true
-        calenderDateView.layoutSubviews()
-        calenderDateView.layoutIfNeeded()
-        calenderView.addSubview(calenderDateView)
-        calenderDateView.loadView()
-        
     }
     
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        if calenderDateView == nil {
+            calenderDateView = Bundle.main.loadNibNamed("ASCalenderView", owner: self, options: nil)?.first as? ASCalenderView
+            calenderDateView.backgroundColor = UIColor.darkGray
+            calenderDateView.frame = CGRect(x: 0, y: 0, width: calenderView.frame.width, height: calenderView.frame.size.height)
+            calenderDateView?.translatesAutoresizingMaskIntoConstraints = true
+            calenderDateView.layoutSubviews()
+            calenderView.setNeedsLayout()
+            calenderDateView.layoutIfNeeded()
+            calenderDateView.loadView()
+            calenderView.addSubview(calenderDateView)
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
